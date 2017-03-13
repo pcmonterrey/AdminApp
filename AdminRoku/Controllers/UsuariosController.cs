caabf9c28ae16401c47ee882c9bbd1c81776f9a3
FabianVega <Roberto@USER>
2017-03-12 20:20:46 -0600

    Inicio de solucion

---
+++ AdminRoku/Controllers/UsuariosController.cs
@@ -0,0 +1,129 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data;
+using System.Data.Entity;
+using System.Linq;
+using System.Threading.Tasks;
+using System.Net;
+using System.Web;
+using System.Web.Mvc;
+using AdminRoku.Models;
+using DAL;
+
+namespace AdminRoku.Controllers
+{
+    public class UsuariosController : Controller
+    {
+        private ApplicationDbContext db = new ApplicationDbContext();
+
+        // GET: Usuarios
+        public async Task<ActionResult> Index()
+        {
+            return View(await db.Usuarios.ToListAsync());
+        }
+
+        // GET: Usuarios/Details/5
+        public async Task<ActionResult> Details(int? id)
+        {
+            if (id == null)
+            {
+                return new HttpStatusCodeResult(HttpStatusCode.BadRequest);
+            }
+            Usuarios usuarios = await db.Usuarios.FindAsync(id);
+            if (usuarios == null)
+            {
+                return HttpNotFound();
+            }
+            return View(usuarios);
+        }
+
+        // GET: Usuarios/Create
+        public ActionResult Create()
+        {
+            return View();
+        }
+
+        // POST: Usuarios/Create
+        // Para protegerse de ataques de publicación excesiva, habilite las propiedades específicas a las que desea enlazarse. Para obtener 
+        // más información vea http://go.microsoft.com/fwlink/?LinkId=317598.
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public async Task<ActionResult> Create([Bind(Include = "Id,Nombre,Usuario,Contrasena,Estado,FechaCreacion,FechaModificacion")] Usuarios usuarios)
+        {
+            if (ModelState.IsValid)
+            {
+                db.Usuarios.Add(usuarios);
+                await db.SaveChangesAsync();
+                return RedirectToAction("Index");
+            }
+
+            return View(usuarios);
+        }
+
+        // GET: Usuarios/Edit/5
+        public async Task<ActionResult> Edit(int? id)
+        {
+            if (id == null)
+            {
+                return new HttpStatusCodeResult(HttpStatusCode.BadRequest);
+            }
+            Usuarios usuarios = await db.Usuarios.FindAsync(id);
+            if (usuarios == null)
+            {
+                return HttpNotFound();
+            }
+            return View(usuarios);
+        }
+
+        // POST: Usuarios/Edit/5
+        // Para protegerse de ataques de publicación excesiva, habilite las propiedades específicas a las que desea enlazarse. Para obtener 
+        // más información vea http://go.microsoft.com/fwlink/?LinkId=317598.
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public async Task<ActionResult> Edit([Bind(Include = "Id,Nombre,Usuario,Contrasena,Estado,FechaCreacion,FechaModificacion")] Usuarios usuarios)
+        {
+            if (ModelState.IsValid)
+            {
+                db.Entry(usuarios).State = EntityState.Modified;
+                await db.SaveChangesAsync();
+                return RedirectToAction("Index");
+            }
+            return View(usuarios);
+        }
+
+        // GET: Usuarios/Delete/5
+        public async Task<ActionResult> Delete(int? id)
+        {
+            if (id == null)
+            {
+                return new HttpStatusCodeResult(HttpStatusCode.BadRequest);
+            }
+            Usuarios usuarios = await db.Usuarios.FindAsync(id);
+            if (usuarios == null)
+            {
+                return HttpNotFound();
+            }
+            return View(usuarios);
+        }
+
+        // POST: Usuarios/Delete/5
+        [HttpPost, ActionName("Delete")]
+        [ValidateAntiForgeryToken]
+        public async Task<ActionResult> DeleteConfirmed(int id)
+        {
+            Usuarios usuarios = await db.Usuarios.FindAsync(id);
+            db.Usuarios.Remove(usuarios);
+            await db.SaveChangesAsync();
+            return RedirectToAction("Index");
+        }
+
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing)
+            {
+                db.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+    }
+}
---
+++ AdminRoku/Controllers/UsuariosServiciosController.cs
@@ -0,0 +1,129 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data;
+using System.Data.Entity;
+using System.Linq;
+using System.Threading.Tasks;
+using System.Net;
+using System.Web;
+using System.Web.Mvc;
+using AdminRoku.Models;
+using DAL;
+
+namespace AdminRoku.Controllers
+{
+    public class UsuariosServiciosController : Controller
+    {
+        private ApplicationDbContext db = new ApplicationDbContext();
+
+        // GET: UsuariosServicios
+        public async Task<ActionResult> Index()
+        {
+            return View(await db.UsuariosServicios.ToListAsync());
+        }
+
+        // GET: UsuariosServicios/Details/5
+        public async Task<ActionResult> Details(int? id)
+        {
+            if (id == null)
+            {
+                return new HttpStatusCodeResult(HttpStatusCode.BadRequest);
+            }
+            UsuariosServicios usuariosServicios = await db.UsuariosServicios.FindAsync(id);
+            if (usuariosServicios == null)
+            {
+                return HttpNotFound();
+            }
+            return View(usuariosServicios);
+        }
+
+        // GET: UsuariosServicios/Create
+        public ActionResult Create()
+        {
+            return View();
+        }
+
+        // POST: UsuariosServicios/Create
+        // Para protegerse de ataques de publicación excesiva, habilite las propiedades específicas a las que desea enlazarse. Para obtener 
+        // más información vea http://go.microsoft.com/fwlink/?LinkId=317598.
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public async Task<ActionResult> Create([Bind(Include = "Id,IdServicio,IdUsario,Estado,FechaCreacion,FechaModificacion,FechaInicioServicio,FechaFinServicio,NumeroCreditos")] UsuariosServicios usuariosServicios)
+        {
+            if (ModelState.IsValid)
+            {
+                db.UsuariosServicios.Add(usuariosServicios);
+                await db.SaveChangesAsync();
+                return RedirectToAction("Index");
+            }
+
+            return View(usuariosServicios);
+        }
+
+        // GET: UsuariosServicios/Edit/5
+        public async Task<ActionResult> Edit(int? id)
+        {
+            if (id == null)
+            {
+                return new HttpStatusCodeResult(HttpStatusCode.BadRequest);
+            }
+            UsuariosServicios usuariosServicios = await db.UsuariosServicios.FindAsync(id);
+            if (usuariosServicios == null)
+            {
+                return HttpNotFound();
+            }
+            return View(usuariosServicios);
+        }
+
+        // POST: UsuariosServicios/Edit/5
+        // Para protegerse de ataques de publicación excesiva, habilite las propiedades específicas a las que desea enlazarse. Para obtener 
+        // más información vea http://go.microsoft.com/fwlink/?LinkId=317598.
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public async Task<ActionResult> Edit([Bind(Include = "Id,IdServicio,IdUsario,Estado,FechaCreacion,FechaModificacion,FechaInicioServicio,FechaFinServicio,NumeroCreditos")] UsuariosServicios usuariosServicios)
+        {
+            if (ModelState.IsValid)
+            {
+                db.Entry(usuariosServicios).State = EntityState.Modified;
+                await db.SaveChangesAsync();
+                return RedirectToAction("Index");
+            }
+            return View(usuariosServicios);
+        }
+
+        // GET: UsuariosServicios/Delete/5
+        public async Task<ActionResult> Delete(int? id)
+        {
+            if (id == null)
+            {
+                return new HttpStatusCodeResult(HttpStatusCode.BadRequest);
+            }
+            UsuariosServicios usuariosServicios = await db.UsuariosServicios.FindAsync(id);
+            if (usuariosServicios == null)
+            {
+                return HttpNotFound();
+            }
+            return View(usuariosServicios);
+        }
+
+        // POST: UsuariosServicios/Delete/5
+        [HttpPost, ActionName("Delete")]
+        [ValidateAntiForgeryToken]
+        public async Task<ActionResult> DeleteConfirmed(int id)
+        {
+            UsuariosServicios usuariosServicios = await db.UsuariosServicios.FindAsync(id);
+            db.UsuariosServicios.Remove(usuariosServicios);
+            await db.SaveChangesAsync();
+            return RedirectToAction("Index");
+        }
+
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing)
+            {
+                db.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+    }
+}
