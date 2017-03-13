7e307aa107ee94b17faf3a01f855f71127da634e
J Roberto Vega <pcmonterrey@hotmail.com>
2017-03-13 16:29:53 -0600

    faltaba este archivo

---
+++ AdminRoku/Controllers/BaseController.cs
@@ -0,0 +1,17 @@
+﻿using AdminRoku.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace AdminRoku.Controllers
+{
+    public class BaseController : Controller
+    {        
+        public BLL.AdminRoku oBussiness;
+        public BaseController(){
+            oBussiness = new BLL.AdminRoku();
+        }
+    }
+}
