c19ae796e87fccb3afe116d6e89c1969cff1176f
J Roberto Vega <pcmonterrey@hotmail.com>
2017-03-14 11:57:51 -0600

    Se agrega ejmplo de como usar los recursos, se cambia DAL. por Model. en vistas

--- Resources/AdminRokuResourcesGlobal.cs
+++ Resources/AdminRokuResourcesGlobal.cs
@@ -2,9 +2,11 @@
 
 namespace Resources
 {
-    public static class AdminRokuGlobal
+    public static class AdminRokuResourcesGlobal
     {
     	
+		public static string linkEditar  { get { return AdminRokuResourceManager.GetKeyValue("linkEditar"); } }
+   	
 		public static string textoBtnGuardar  { get { return AdminRokuResourceManager.GetKeyValue("textoBtnGuardar"); } }
    	
 		public static string textoBtnOk  { get { return AdminRokuResourceManager.GetKeyValue("textoBtnOk"); } }
