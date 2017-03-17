11006fb9bf2aeb7ecf5796852e7c0b185eff90a2
FabianVega <Roberto@USER>
2017-03-17 14:40:43 -0600

    Agregados los SP de usuarios

--- DAL/AdminRokuADO.Designer.cs
+++ DAL/AdminRokuADO.Designer.cs
@@ -1,4 +1,4 @@
-﻿// T4 code generation is enabled for model 'F:\GitDemos\AdminApp\DAL\AdminRokuADO.edmx'. 
+﻿// T4 code generation is enabled for model 'C:\Users\Roberto\Source\Repos\AdminApp\DAL\AdminRokuADO.edmx'. 
 // To enable legacy code generation, change the value of the 'Code Generation Strategy' designer
 // property to 'Legacy ObjectContext'. This property is available in the Properties Window when the model
 // is open in the designer.
